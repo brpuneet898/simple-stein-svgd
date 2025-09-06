20bcf7ed1b149b4b58468e976db05b54835cc0e3
Puneet <90244927+brpuneet898@users.noreply.github.com>
2025-09-06 20:39:01 +0530

    Delete simple-stein-svgd directory

--- simple-stein-svgd/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Simple-Stein-SVGD: A minimal Python package for Stein Variational Gradient Descent
-"""
-
-from .svgd import SVGD
-from .kernels import rbf_kernel
-
-__all__ = ["SVGD", "rbf_kernel"]
